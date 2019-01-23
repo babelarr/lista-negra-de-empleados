108fcba08efe9cb2af7a9a6269997664d57232a5
babelarr <babelarr@gmail.com>
2019-01-23 22:11:02 +0100

    add filter

--- src/App.js
+++ src/App.js
@@ -9,11 +9,31 @@ class App extends Component {
     super(props);
 
     this.state = {
+      query: '',
       results: this.getSavedData()
-    }
+    };
+    this.getQuery = this.getQuery.bind(this);
 
   }
 
+  getQuery(e) {
+    const userQuery= e.currentTarget.value;
+    this.setState({
+      query: userQuery
+    });
+  }
+
+  filterThis() {
+    const filteredResults = this.state.results.filter(item => {
+      const fullName = `${item.name.first} ${item.name.last}`;
+      if(fullName.toLowerCase().includes(this.state.query.toLowerCase())) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+    return filteredResults;
+  }
 
   getSavedData() {
     const blackData = localStorage.getItem('blackData');
@@ -44,23 +64,34 @@ class App extends Component {
   }
 
   render() {
+    const blackResults = this.filterThis();
     return (
       <div className="app">
-      <h1 className="app__title">Lista negra de empleados <span role="img" aria-label="GRRR">😡</span></h1>
-      <ul className="app__list">
-        {this.state.results.map(item => {
-          return (
-            <li className="app__list-item" key={item.id}>
-              <div className="person">
-                <h2 className="person__name">{`${item.name.first} ${item.name.last}`}</h2>
-                <img className="person__image" src={item.picture.medium} alt={`${item.name.first} ${item.name.last}`}/>
-                <div className="person__age">{item.dob.age}</div>
-                <div className="person__city">{item.location.city}</div>
-              </div>
-            </li>
-          )
-        })}
-      </ul>
+      <header className=".app__header">
+        <h1 className="app__title">Lista negra de empleados <span role="img" aria-label="GRRR">😡</span></h1>
+        <div className="app__filter">
+          <div className="app__fitler-item">
+            <input type="text" onKeyUp={this.getQuery} className="app__filter-full-name" placeholder="Busca a los culpables💀"/>
+          </div>
+        </div>
+      </header>
+
+      <main className="app__main">
+        <ul className="app__list">
+          {blackResults.map(item => {
+            return (
+              <li className="app__list-item" key={item.id}>
+                <div className="person">
+                  <h2 className="person__name">{`${item.name.first} ${item.name.last}`}</h2>
+                  <img className="person__image" src={item.picture.medium} alt={`${item.name.first} ${item.name.last}`}/>
+                  <div className="person__age">{item.dob.age}</div>
+                  <div className="person__city">{item.location.city}</div>
+                </div>
+              </li>
+            )
+          })}
+        </ul>
+      </main>
       </div>
     );
   }
