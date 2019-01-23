2cffcc824d4906d60bdeaed0fe1f6463879cb016
babelarr <babelarr@gmail.com>
2019-01-23 20:43:52 +0100

    add localstorage

--- src/App.js
+++ src/App.js
@@ -9,10 +9,25 @@ class App extends Component {
     super(props);
 
     this.state = {
-      results: []
+      results: this.getSavedData()
     }
 
-    this.getBlackCriaturas();
+  }
+
+
+  getSavedData() {
+    const blackData = localStorage.getItem('blackData');
+
+    if (blackData !== null) {
+      return JSON.parse(blackData);
+    } else {
+      this.getBlackCriaturas();
+      return [];
+    }
+  }
+
+  saveData(data) {
+    localStorage.setItem('blackData', JSON.stringify(data));
   }
 
   getBlackCriaturas() {
@@ -24,27 +39,28 @@ class App extends Component {
       this.setState({
         results: cleanData
       });
+      this.saveData(cleanData);
     });
   }
 
   render() {
     return (
       <div className="app">
-      <h1 className="app__title">Lista negra de empleados</h1>
-        <ul className="app__list">
-          {this.state.results.map(item => {
-            return (
-              <li className="app__list-item" key={item.id}>
-                <div className="person">
-                  <h2 className="person__name">{`${item.name.first} ${item.name.last}`}</h2>
-                  <img className="person__image" src={item.picture.medium} alt={`${item.name.first} ${item.name.last}`}/>
-                  <div className="person__age">{item.dob.age}</div>
-                  <div className="person__city">{item.location.city}</div>
-                </div>
-              </li>
-            )
-          })}
-        </ul>
+      <h1 className="app__title">Lista negra de empleados <span role="img" aria-label="GRRR">😡</span></h1>
+      <ul className="app__list">
+        {this.state.results.map(item => {
+          return (
+            <li className="app__list-item" key={item.id}>
+              <div className="person">
+                <h2 className="person__name">{`${item.name.first} ${item.name.last}`}</h2>
+                <img className="person__image" src={item.picture.medium} alt={`${item.name.first} ${item.name.last}`}/>
+                <div className="person__age">{item.dob.age}</div>
+                <div className="person__city">{item.location.city}</div>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
       </div>
     );
   }
